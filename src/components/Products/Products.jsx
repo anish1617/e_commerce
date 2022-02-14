37eb633500645f09deee44c5c18e312fe310b39c
Anish Mandal <basketb523@gmail.com>
2022-02-14 17:30:07 +0545

    Added Product and Navbar components

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import Product from "./Product/Product";
+import useStyles from "./styles";
+const products = [
+  {
+    id: 1,
+    name: "Shoes",
+    description: "Running Shoes",
+    price: "$5",
+    image: "https://pixabay.com/photos/macbook-laptop-google-display-459196/",
+  },
+  {
+    id: 2,
+    name: "MacBook",
+    description: "MacBook Laptop",
+    price: "$10",
+    image: "https://pixabay.com/photos/macbook-laptop-google-display-459196/",
+  },
+];
+
+const Products = () => {
+  const classes = useStyles();
+  return (
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
+      <Grid container justifyContent="center" spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
+};
+
+export default Products;
