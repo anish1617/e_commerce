058eca36b2012a5e82a1bb73888fda3098c2d7b2
anish1617 <basketb523@gmail.com>
2022-02-15 18:47:20 +0545

    Added Cart Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { commerce } from "./lib/commerce";
 
-import { Products, Navbar } from "./components";
+import { Products, Navbar, Cart } from "./components";
+
 const App = () => {
   const [products, setProducts] = useState([]);
 
@@ -35,7 +36,8 @@ const App = () => {
   return (
     <>
       <Navbar totalItems={cart.total_items} />
-      <Products products={products} onAddToCart={handleAddToCart} />
+      {/* <Products products={products} onAddToCart={handleAddToCart} /> */}
+      <Cart cart={cart} />
     </>
   );
 };
